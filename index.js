3418a119e56a78ec85f662b4e5f53008492016df
CharlesOsang017 <charlesosango02@gmail.com>
2025-03-08 17:00:48 +0300

    Allowed origin

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://trace-zeta.vercel.app/",
     credentials: true,
   })
 );
