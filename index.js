a8240088ce2f0674a571f63b2535c3c18e51e6c1
CharlesOsang017 <charlesosango02@gmail.com>
2025-03-08 15:59:45 +0300

    deployment error

--- index.js
+++ index.js
@@ -1,53 +1,32 @@
-import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-
 import { connectToDb } from "./config/db.js";
 import authRoutes from "./routes/user.route.js";
 import issueRoutes from "./routes/issue.route.js";
+import express from "express";
 
-// Load environment variables first
+import dotenv from "dotenv";
+
 dotenv.config();
-
-// Create an Express application
 const app = express();
 
-// Middleware configuration
-const middleware = [
-  express.json(),
-  cookieParser(),
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
-  }),
-];
+  })
+);
 
-app.use(middleware);
-
-// Routes configuration
-const routes = [
-  { path: "/api/v1/users", handler: authRoutes },
-  { path: "/api/v1/issues", handler: issueRoutes },
-];
-
-routes.forEach((route) => app.use(route.path, route.handler));
-
-// Port configuration
+// routes
+app.use("/api/v1/users", authRoutes);
+app.use("/api/v1/issues", issueRoutes);
 const PORT = process.env.PORT || 5000;
 
-// Server startup
-const startServer = async () => {
-  try {
-    console.log(`Connecting to the database...`);
-    await connectToDb(); // Ensure DB is connected first
-    console.log(`Server is running on port ${PORT}`);
-
-    app.listen(PORT);
-  } catch (error) {
-    console.error("Error starting the server:", error);
-  }
-};
-
 // Start the server
-startServer();
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+  await connectToDb();
+});
